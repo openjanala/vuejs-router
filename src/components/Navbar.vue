b22165091c706857d2358d4a18b30f1b394b902f
openjanala <openjanala@gmail.com>
2020-05-21 12:43:26 +0600

    Class 24 completed of Router Link

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/About">About</router-link></li>
+                <li><router-link :to="{name:'Contact'}">Contact</router-link></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script >
+export default {
+    name:'Navbar',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+nav{
+    text-align: center;
+}
+nav ul{
+    padding:0;
+}
+nav li{
+    display:inline-block;
+    padding:20px 10px;
+     
+}
+nav a {
+    text-transform: uppercase;
+    text-decoration: none;
+}
+</style>
